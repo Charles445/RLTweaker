ae412759e7a2f7478847f01aef749b639ce205b4
Charles445 <charlesd@wenzel.com>
2022-02-18 20:41:14 -0600

    PotionCore Jump Ordering and Fixes
    Run potion core jump handling runs before other events
    Also fix issues with invalid jump boosts

--- src/main/java/com/charles445/rltweaker/handler/PotionCoreHandler.java
+++ src/main/java/com/charles445/rltweaker/handler/PotionCoreHandler.java
@@ -7,12 +7,18 @@ import com.charles445.rltweaker.util.CriticalException;
 import com.charles445.rltweaker.util.ErrorUtil;
 
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.MobEffects;
 import net.minecraft.potion.PotionEffect;
+import net.minecraft.world.World;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.living.LivingEvent;
+import net.minecraftforge.event.entity.living.PotionEvent.PotionApplicableEvent;
+import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.IEventListener;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent.PlayerTickEvent;
 
 public class PotionCoreHandler
 {
@@ -48,7 +54,8 @@ public class PotionCoreHandler
 			MinecraftForge.EVENT_BUS.register(this);
 		}
 		
-		@SubscribeEvent
+		//Running late will override other jump events
+		@SubscribeEvent(priority = EventPriority.HIGHEST)
 		public void onJump(LivingEvent.LivingJumpEvent event)
 		{
 			EntityLivingBase living = event.getEntityLiving();
@@ -59,11 +66,79 @@ public class PotionCoreHandler
 			if (jumpEffect != null && (jumpEffect.getAmplifier() < 0 || jumpEffect.getAmplifier() > 127))
 			{
 				if(living.world.isRemote)
+				{
 					living.getEntityData().setBoolean("Potion Core - Jump Boost Jumping", false);
+				}
+				
 				return;
 			}
 			
 			handler.invoke(event);
+			
+			//Clean up jump effect on client if necessary
+			if(jumpEffect == null && living.world.isRemote)
+			{
+				living.getEntityData().setBoolean("Potion Core - Jump Boost Jumping", false);
+			}
+		}
+		
+		@SubscribeEvent
+		public void onPlayerUpdate(PlayerTickEvent event)
+		{
+			if(event.phase == TickEvent.Phase.END)
+				return;
+			
+			EntityPlayer player = event.player;
+			
+			if(player == null)
+				return;
+			
+			World world = player.world;
+			
+			if(world == null)
+				return;
+			
+			if(!world.isRemote)
+				return;
+			
+			double jbh = event.player.getEntityData().getDouble("Potion Core - Jump Boost Height");
+			
+			if(jbh > 127)
+			{
+				ErrorUtil.logSilent("PotionCore Invalid Jump Boost Height");
+				event.player.getEntityData().setDouble("Potion Core - Jump Boost Height", 1.75D);
+			}
+		}
+		
+		@SubscribeEvent
+		public void potionApplicable(PotionApplicableEvent event)
+		{
+			EntityLivingBase living = event.getEntityLiving();
+			PotionEffect newEffect = event.getPotionEffect();
+			
+			if(newEffect == null)
+				return;
+			
+			if(!(living instanceof EntityPlayer))
+				return;
+			
+			if(!living.world.isRemote)
+				return;
+			
+			//Client only
+			
+			if(newEffect.getPotion() == MobEffects.JUMP_BOOST)
+			{
+				PotionEffect oldEffect = living.getActivePotionEffect(MobEffects.JUMP_BOOST);
+				if(oldEffect == null)
+					return;
+				
+				if(newEffect.getAmplifier() < 0 && oldEffect.getAmplifier() >= 0)
+				{
+					//Got sent a negative number from server, remove the potion effect to let it through
+					living.removePotionEffect(MobEffects.JUMP_BOOST);
+				}
+			}
 		}
 	}
 }
